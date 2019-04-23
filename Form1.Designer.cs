70705adad4fc0d0f32aecbb36d07553d4028ee4c
Shawn Jass <shawnjass@gmail.com>
2019-04-23 14:46:53 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dataGridUser = new System.Windows.Forms.DataGridView();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dataGridLineItem = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.txtDateAdded = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridLineItem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(138, 453);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(112, 37);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Blue;
+            this.btnSave.Location = new System.Drawing.Point(3, 453);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 37);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSearch.Location = new System.Drawing.Point(662, 33);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(112, 37);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dataGridUser
+            // 
+            this.dataGridUser.AllowUserToAddRows = false;
+            this.dataGridUser.AllowUserToDeleteRows = false;
+            this.dataGridUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridUser.Location = new System.Drawing.Point(399, 82);
+            this.dataGridUser.Name = "dataGridUser";
+            this.dataGridUser.ReadOnly = true;
+            this.dataGridUser.Size = new System.Drawing.Size(506, 216);
+            this.dataGridUser.TabIndex = 1;
+            this.dataGridUser.DoubleClick += new System.EventHandler(this.dataGridUser_DoubleClick);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(177, 82);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(188, 29);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(177, 126);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(188, 29);
+            this.txtAddress.TabIndex = 2;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(177, 171);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(188, 69);
+            this.txtDescription.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(26, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "BILL TO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Description";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 451);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(399, 38);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(249, 29);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // dataGridLineItem
+            // 
+            this.dataGridLineItem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridLineItem.Location = new System.Drawing.Point(399, 304);
+            this.dataGridLineItem.Name = "dataGridLineItem";
+            this.dataGridLineItem.Size = new System.Drawing.Size(506, 186);
+            this.dataGridLineItem.TabIndex = 4;
+            this.dataGridLineItem.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridLineItem_CellContentClick);
+            this.dataGridLineItem.DoubleClick += new System.EventHandler(this.dataGridLineItem_DoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 284);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Qty";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 320);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 21);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Date Added";
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(177, 284);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(100, 29);
+            this.txtQty.TabIndex = 6;
+            // 
+            // txtDateAdded
+            // 
+            this.txtDateAdded.Location = new System.Drawing.Point(177, 317);
+            this.txtDateAdded.Name = "txtDateAdded";
+            this.txtDateAdded.Size = new System.Drawing.Size(100, 29);
+            this.txtDateAdded.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 502);
+            this.Controls.Add(this.txtDateAdded);
+            this.Controls.Add(this.txtQty);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridLineItem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.dataGridUser);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDelete);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "MySqlCrud App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridLineItem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dataGridUser;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dataGridLineItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.TextBox txtDateAdded;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=localhost;Database=applicant_test;Uid=root;Pwd=Password#13;";
+        //if userID is 0 it will insert a new record if not it will update corresponding record
+        int userID = 0;
+        int lineItemID = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("UserAddOrEdit", mysqlCon);
+                MySqlCommand mySqlCmd2 = new MySqlCommand("LineItemsViewAll", mysqlCon);
+
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd2.CommandType = CommandType.StoredProcedure;
+
+                //userID is variable parm which captures the data to be saved if 0 and update if not
+                mySqlCmd.Parameters.AddWithValue("_UserID", userID);
+                mySqlCmd.Parameters.AddWithValue("_UserName", txtUserName.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_Address", txtAddress.Text.Trim());
+                mySqlCmd.ExecuteNonQuery();
+
+                mySqlCmd2.Parameters.AddWithValue("_LineItemID", lineItemID);
+                mySqlCmd2.Parameters.AddWithValue("_Description", txtDescription.Text.Trim());
+                mySqlCmd2.Parameters.AddWithValue("_Qty", txtQty.Text.Trim());
+                mySqlCmd2.Parameters.AddWithValue("_DateAdded", txtDateAdded.Text.Trim());
+                mySqlCmd2.ExecuteNonQuery();
+
+                MessageBox.Show("Successfully Submitted");
+                Clear();
+                ViewGrid();
+                ViewGrid2();
+            }
+        }
+
+        void ViewGrid()
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("UserViewAll",mysqlCon);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dtblUser = new DataTable();
+                sqlDa.Fill(dtblUser);
+                dataGridUser.DataSource = dtblUser;
+                dataGridUser.Columns[0].Visible = false;
+            }
+        }
+
+        void ViewGrid2()
+        {
+            using (MySqlConnection mysqlCon2 = new MySqlConnection(connectionString))
+            {
+                mysqlCon2.Open();
+                MySqlDataAdapter sqlDa2 = new MySqlDataAdapter("LineItemViewAll", mysqlCon2);
+                sqlDa2.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dtblLine_Item = new DataTable();
+                sqlDa2.Fill(dtblLine_Item);
+                dataGridLineItem.DataSource = dtblLine_Item;
+                dataGridLineItem.Columns[0].Visible = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            ViewGrid();
+            ViewGrid2();
+        }
+
+        void Clear()
+        {
+            txtUserName.Text = txtAddress.Text = txtDescription.Text = txtSearch.Text = txtQty.Text = txtDateAdded.Text = "";
+            userID = 0;
+            lineItemID = 0;
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+        }
+
+        private void dataGridUser_DoubleClick(object sender, EventArgs e)
+        {
+            if(dataGridUser.CurrentRow.Index != -1)
+            {
+                txtUserName.Text = dataGridUser.CurrentRow.Cells[1].Value.ToString();
+                txtAddress.Text = dataGridUser.CurrentRow.Cells[2].Value.ToString();
+                userID = Convert.ToInt32(dataGridUser.CurrentRow.Cells[0].Value.ToString());
+                btnSave.Text = "Update";
+                btnDelete.Enabled = Enabled;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("UserSearchByValue", mysqlCon);                
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlDa.SelectCommand.Parameters.AddWithValue("_SearchValue",txtSearch.Text);
+                DataTable dtblUser = new DataTable();
+                sqlDa.Fill(dtblUser);
+                dataGridUser.DataSource = dtblUser;
+                dataGridUser.Columns[0].Visible = false;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("UserDeleteByID", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                //userID is variable parm which captures the data to be saved if 0 and update if not
+                mySqlCmd.Parameters.AddWithValue("_UserID", userID);               
+                mySqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Successfully Deleted");
+                Clear();
+                ViewGrid();
+            }
+        }
+
+        private void dataGridLineItem_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
+        }
+
+        private void dataGridLineItem_DoubleClick(object sender, EventArgs e)
+        {
+            if (dataGridLineItem.CurrentRow.Index != -1)
+            {
+                txtQty.Text = dataGridLineItem.CurrentRow.Cells[1].Value.ToString();
+                txtDateAdded.Text = dataGridLineItem.CurrentRow.Cells[2].Value.ToString();
+                lineItemID = Convert.ToInt32(dataGridLineItem.CurrentRow.Cells[0].Value.ToString());
+                btnSave.Text = "Update";
+                btnDelete.Enabled = Enabled;
+            }
+        }
+    }
+}
+
